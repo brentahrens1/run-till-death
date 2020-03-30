209b4cc3fc2081554040f11ae16cc1861a2d3f4e
Michael <msiller455@gmail.com>
2020-03-30 14:07:18 -0700

    refactors some of navbar with styled components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./NavBar.css"
+
+import { routes } from '../../const/routes'
+
+import {
+    NavContainer,
+    NavLogo,
+    NavList,
+    NavItems
+} from './style'
 
 
 const NavBar = () => {
     return (
-        <div className="nav">
-            <div className="nav--logo">
+        // <div className="nav" id="NavContainer">
+        //     <div className="nav--logo" id="NavLeft">
+        //         <h1><Link to="/">Run Till Death</Link></h1>
+        //     </div>
+        //     <div className="nav--list" id="NavRight">
+        //         <ul className="nav__items">
+        //             <li className="nav__item">About</li>
+        //             <li className="nav__item">Coaching</li>
+        //             <li className="nav__item">Members</li>
+        //             <li className="nav__item">Events</li>
+        //             <li className="nav__item">Elite</li>
+        //             <li className="nav__item">Join</li>
+        //         </ul>
+        //     </div>
+        // </div>
+        <NavContainer>
+            <NavLogo>
                 <h1><Link to="/">Run Till Death</Link></h1>
-            </div>
-            <div className="nav--list">
-                <ul className="nav__items">
-                    <li className="nav__item">About</li>
-                    <li className="nav__item">Coaching</li>
-                    <li className="nav__item">Members</li>
-                    <li className="nav__item">Events</li>
-                    <li className="nav__item">Elite</li>
-                    <li className="nav__item">Join</li>
-                </ul>
-            </div>
-        </div>
+            </NavLogo>
+            <NavList>
+                <NavItems>
+                    {
+                        routes.map(route => <p>{route.text}</p>)
+                    }
+                </NavItems>
+            </NavList>
+        </NavContainer>
     )
 }
 
