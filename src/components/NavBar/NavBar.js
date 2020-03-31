8ee4a133b06a4e694ae97f4d45d90e966ce4203c
Brent Ahrens <cidida@gmail.com>
2020-03-30 17:48:21 -0700

    finishes mobile navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,33 +2,25 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./NavBar.css"
 
+
 import { routes } from '../../const/routes'
 
 import {
     NavContainer,
     NavLogo,
     NavList,
-    NavItems
+    NavItems,
+    NavBarLink,
+    HamburgerContainer,
+    HamburgerBar,
+    Overlay
 } from './style'
 
 
 const NavBar = () => {
+    const [ isOpen, setIsOpen ] = useState(false)
+
     return (
-        // <div className="nav" id="NavContainer">
-        //     <div className="nav--logo" id="NavLeft">
-        //         <h1><Link to="/">Run Till Death</Link></h1>
-        //     </div>
-        //     <div className="nav--list" id="NavRight">
-        //         <ul className="nav__items">
-        //             <li className="nav__item">About</li>
-        //             <li className="nav__item">Coaching</li>
-        //             <li className="nav__item">Members</li>
-        //             <li className="nav__item">Events</li>
-        //             <li className="nav__item">Elite</li>
-        //             <li className="nav__item">Join</li>
-        //         </ul>
-        //     </div>
-        // </div>
         <NavContainer>
             <NavLogo>
                 <h1><Link to="/">Run Till Death</Link></h1>
@@ -36,12 +28,27 @@ const NavBar = () => {
             <NavList>
                 <NavItems>
                     {
-                        routes.map(route => <p>{route.text}</p>)
+                        routes.map(route => <NavBarLink exact to={route.path}>{route.text}</NavBarLink>)
                     }
                 </NavItems>
             </NavList>
+            <Hamburger setIsOpen={setIsOpen} isOpen={isOpen} />
+            <Overlay className={ isOpen ? "show" : "hide"}>
+                    {
+                        routes.map(route => 
+                            <NavBarLink className="overlay-links" exact to={route.path}>{route.text}</NavBarLink>
+                        )
+                    }
+            </Overlay>
         </NavContainer>
     )
 }
+
+const Hamburger = ({ setIsOpen, isOpen }) => 
+    <HamburgerContainer className={isOpen ? "open" : "closed"} onClick={() => setIsOpen(!isOpen)}>
+        <HamburgerBar></HamburgerBar>
+        <HamburgerBar></HamburgerBar>
+        <HamburgerBar></HamburgerBar>
+    </HamburgerContainer>
 
 export default NavBar
--- src/components/NavBar/style.js
+++ src/components/NavBar/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
 export const NavContainer = styled.div`
@@ -19,13 +19,106 @@ export const NavLogo = styled.div`
 `
 
 export const NavList = styled.div`
-    width: 35%;
+    width: 45%;
     font-size: 17.6px;
+    @media (max-width: 950px) {
+        a {
+            display: none;
+        }
+    }
 `
 
 export const NavItems = styled.ul`
     display: flex;
+    position: relative;
     flex-direction: row;
     list-style: none;
     justify-content: space-around;
 `
+
+export const NavBarLink = styled(NavLink)`
+    font-weight: 700;
+    display: inline-block;
+    &::after {
+        content: '';
+        display: block;
+        background: #fff;
+        width: 0;
+        height: 2px;
+        transition: width .3s;
+        margin-top: .2rem;
+    }
+    &:hover::after {
+        width: 100%;
+    }
+`
+
+export const HamburgerContainer = styled.div`
+    width: 40px;
+    height: 25px;
+    position: relative;
+    display: none;
+    margin-left: auto;
+    cursor: pointer;
+    z-index: 1000;
+    @media (max-width: 950px) {
+        display: block;
+    }
+`
+
+export const HamburgerBar = styled.div`
+    position: absolute;
+    width: 25px;
+    height: 3px;
+    background: #fff;
+    left: 0;
+    transition: all .50s ease-in-out;
+    &:nth-child(1) {
+        top: 0;
+    }
+    &:nth-child(2) {
+        top: 6px;
+    }
+    &:last-child {
+        top: 12px;
+    }
+    .open > & {
+        transition: all 1s ease-in-out;
+    }
+    .open > &:first-child {
+        top: 45%;
+        transform: rotate(135deg);
+        transition: all .50s ease-in-out;
+    }
+    .open > &:nth-child(2) {
+        opacity: 0;
+        left: -60px;
+        transition: all .50s ease-in-out;
+    }
+    .open > &:nth-child(3) {
+        transform: rotate(-135deg);
+        transition: all .50s ease-in-out;
+    }
+`
+
+export const Overlay = styled.div`
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    background: #000;
+    left: -120rem;
+    transition: left .75s ease-in-out;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    font-size: 2.5rem;
+    &.show {
+        left: 0;
+    }
+    & > a {
+        margin: .5rem 0;
+    }
+`
+
