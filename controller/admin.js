f0878451f93384ca8496b9a55071b8cad518f9cf
Michael <msiller455@gmail.com>
2020-04-07 14:34:39 -0700

    adds jwt functionality

--- controller/admin.js
+++ controller/admin.js
@@ -1,19 +1,29 @@
 const Admin = require('../models/admin')
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.SECRET
 
 module.exports = {
     signup
 }
 
 async function signup(req, res) {
-    console.log('hello')
     const admin = new Admin(req.body)
     try {
         await admin.save()
-        res.json(admin)
+        const token = createJWT(admin)
+        res.json({ token })
     }
     catch (err) {
         res.status(400).json(err)
     }
+}
+
+function createJWT(admin) {
+    return jwt.sign(
+        {admin},
+        SECRET,
+        {expiresIn: '24h'}
+    )
 }
 
 
