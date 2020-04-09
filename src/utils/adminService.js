c11c075acf32f6c0d92beadb411e03ef441fce28
Michael <msiller455@gmail.com>
2020-04-09 14:56:04 -0700

    adds sign up, logout and starts login functionality

--- src/utils/adminService.js
+++ src/utils/adminService.js
@@ -21,8 +21,26 @@ function getAdmin() {
     return tokenService.getAdminFromToken()
 }
 
+function login(credentials) {
+    return fetch(BASE_URL + 'login', {
+        method: 'POST',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(credentials)
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+        throw new Error('Bad Credentials')
+    })
+    .then(({token}) => tokenService.setToken(token))
+}
+
+function logout() {
+    tokenService.removeToken()
+}
+
 
 export default { 
     signup,
-    getAdmin
+    getAdmin,
+    logout
 }
